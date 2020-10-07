245dd7ad71637191e342639ac441b0c4ac4abe89
pkliui <kliuiev@pm.me>
2020-10-07 22:05:20 +0300

    fixed bugs in phase_alignment_GS method

--- PhaseRetrieval/modules/postprocessing.py
+++ PhaseRetrieval/modules/postprocessing.py
@@ -11,54 +11,17 @@ import os
 from PIL import Image
 import time
 
-def phase_alignment_gerchberg_saxton(amplitude_filename = None,
-                                     phase_filenames = None,
-                                     num_files_to_align = None,
-                                     delimiter = '\t',
-                                     ref_coordinates=None,
-                                     symmetric_phase = True,
-                                     plot_progress=True,
-                                     plot_every_kth_iteration=1,
-                                     zoom=1):
-    """
-    Alignment of phase images yielded by Gerchberg-Saxton algorithm (in csv file format) (of either pbject-domain or Fourier domain phase images).
-    It is assumed that the corresponding amplitude is known and the phases are all in spatial registry.
-    ---
-    Parameters
-    ---
-    amplitude_filename : list
-        Path to file containing amplitude distribution
-        Default is None
-    phase_filenames : list
-        List of paths to files with phase distributions to be aligned
-        Default is None
-    num_files_to_align : int
-        Integer number of files with phase distributions to be aligned
-        If set to None, all files will be used for alignment.
-        Default is None.
-    delimiter : str, optional
-        Delimiter in csv files
-        Default is '\t'
-    ref_coordinates: list [1x2], optional
-        Coordinates of a reference phase value.
-        If set to None, the ref_coordinates will be set the image's centre coordinates (i.e. [501,501] for 1000x1000 pixels image)
-        If a list is provided, the values will be taken from there.
-    symmetric_phase ; bool, optional
-        If set to True, pixel values in the final phase distribution will be shifted symmetrically w.r.t zero
-        If False, pixels values in the final phase distribution will be left as they are after alignment
-        Default is True
-    plot_progress : bool, optional
-        False will prevent algorithm from plotting the progress.
-        True will plot the progress of the algorithm.
-        Default is False.
-    plot_every_kth_iteration : int, optional
-        Plot the progress each k-th iteration, where k=plot_every_kth_iteration
-        Default is 1.
-    zoom: int, optional
-        Zoom factor to zoom into the 2D plot
-        Default is 1 (no zoom)
-    """
 
+def phase_alignment_gerchberg_saxton(amplitude_filename = None,
+                                     delimiter=None,
+                                     phase_filenames = None,
+                                     ref_coordinates = None,
+                                     num_files_to_align = None,
+                                     symmetric_phase = True,
+                                     plot_progress = True,
+                                     plot_every_kth_iteration=1,
+                                     zoom = 1):
+    #
     #read amplitude image
     if os.path.exists(amplitude_filename):
         # read amplitude image
@@ -66,78 +29,69 @@ def phase_alignment_gerchberg_saxton(amplitude_filename = None,
             #amplitude = io.imread(amplitude_filename)
             amplitude = np.asarray(Image.open(amplitude_filename))
         elif amplitude_filename[-4:] == '.csv':
-            amplitude = pd.read_csv(amplitude_filename, delimiter='\t', header=None).values
+            amplitude = pd.read_csv(amplitude_filename, delimiter=delimiter, header=None).values
         else:
             raise ValueError("Data must be either in tif or csv format.")
     else:
         raise ValueError("Path to an amplitude file does not exist")
-    #
+
     for phase_idx, phase_file in enumerate(sorted(phase_filenames)):
-        time.sleep(2)
         #
         # read phase images
-        phase = pd.read_csv(phase_file, delimiter=delimiter, header=None)
-        phase = phase.values
-        #
-        #determine image size and set the reference pixel to one located in the middle of the image
-        if ref_coordinates == None:
-            ref_coordinates = [0,0]
-            ref_coordinates[0] = int(phase.shape[0]) // 2 + 1
-            ref_coordinates[1] = int(phase.shape[0]) // 2 + 1
+        if os.path.exists(phase_file):
+            phase = pd.read_csv(phase_file, delimiter=delimiter, header=None).values
             #
-        #or use given values instead
+        else:
+            raise ValueError("Path to an amplitude file does not exist")
+        #
+        #compute reference coordinates
+        if ref_coordinates == None: #applies to phase_idx==0 only
+            # determine image size and set the reference pixel to the ones located in the middle of the image
+            ref_coordinates = [0,0]
+            ref_coordinates[0] = int(phase.shape[0]) // 2
+            ref_coordinates[1] = int(phase.shape[0]) // 2
+            #
+        #applies to all phase_idx
+        #if phase_idx==0 and ref_coordinates are not None, use values given by user / or if phase_idx>0, use  ref_coordinates computed at phase_idx==0 iteration
         else:
             ref_coordinates = ref_coordinates
         #
-        #
-        #set the number of files to align
+        # set the number of files to align either to total number of files or just a part of thereof
         if num_files_to_align is None:
             len_phase_filenames = len(phase_filenames)
         else:
             len_phase_filenames = num_files_to_align
         #
+        #align phases
         if phase_idx == 0:
-            print(phase_file)
-            # no aligned / reference phases at first
-            reference_phase = np.zeros(phase.shape)
             #
-            # let the first file be the "offset" phase
-            offset_phase = np.copy(phase)
-            #
-            # aligned phases is the sum of reference and offset phases
-            aligned_phases = reference_phase + offset_phase
+            #set the reference phase value to 0 in the first 'aligned' phase distribution
+            aligned_phase = np.copy(phase) - np.copy(phase[ref_coordinates[0], ref_coordinates[1]])
+            offset_phase = np.zeros(aligned_phase.shape)
         #
-        elif phase_idx > 0 and phase_idx < len_phase_filenames - 1:
-            print(phase_file)
-            # set the reference phase to be equal to the sum of already aligned phases
-            reference_phase = np.copy(aligned_phases)
+        elif phase_idx > 0 and phase_idx < len_phase_filenames:
             #
-            # treat the next file as the offset phase  which must be aligned
+            # treat the next phase distribution as the offset phase distribution which must be aligned
             offset_phase = np.copy(phase)
             #
-            # get reference phase value
-            ref_phase_value = np.copy(reference_phase[ref_coordinates[0], ref_coordinates[1]])
+            # get current reference phase value in the offset phase distribution
+            ref_phase_value = np.copy(offset_phase[ref_coordinates[0], ref_coordinates[1]])
+            #
             # shift offset phase distributions depending on the sign of the reference phase value
             if ref_phase_value + np.abs(0.5 * ref_phase_value) < 0:
-                offset_phase = np.abs(offset_phase + np.abs(ref_phase_value))
+                offset_phase = offset_phase + np.abs(ref_phase_value)
             elif ref_phase_value + np.abs(0.5 * ref_phase_value) > 0:
-                offset_phase = np.abs(offset_phase - np.abs(ref_phase_value))
+                offset_phase = offset_phase - np.abs(ref_phase_value)
             else:
-                offset_phase = np.abs(offset_phase)
+                offset_phase = offset_phase
             #
             # obtain the sum of aligned phases by adding phase-shifted offset phase to the reference phase
-            aligned_phases = reference_phase + offset_phase
-        #
-        #compute the average of the aligned phases
-        elif phase_idx > 0 and phase_idx == len_phase_filenames - 1:
-            print(phase_file)
-            print("averaging ")
-            aligned_phases_norm = aligned_phases / len_phase_filenames - int(symmetric_phase) * 0.5 * np.max(np.max(aligned_phases)) / len_phase_filenames
+            aligned_phase = np.copy(aligned_phase) + np.copy(offset_phase)
         # finish alignment
         else:
             break
-            #
-            # plot progress
+        #
+        #plot
         if plot_progress is True:
             if (phase_idx + 1) % plot_every_kth_iteration == 0:
                 #
@@ -151,7 +105,7 @@ def phase_alignment_gerchberg_saxton(amplitude_filename = None,
                 offset_phase_weighted[..., -1] = rescale_intensity(amplitude, out_range=(0, 1))
                 #
                 aligned_phases_weighted = plt.cm.bwr(
-                    rescale_intensity(- aligned_phases_norm.min() + aligned_phases_norm, out_range=(0, 1)))
+                    rescale_intensity(- aligned_phase.min() + aligned_phase, out_range=(0, 1)))
                 aligned_phases_weighted[..., -1] = rescale_intensity(amplitude, out_range=(0, 1))
                 #
                 fig, ax = plt.subplots(1, 2, figsize=(10, 20))
@@ -169,8 +123,8 @@ def phase_alignment_gerchberg_saxton(amplitude_filename = None,
                             theshape[1] // 2 + theshape[1] // 2 // zoom])
                 #
                 # computed Fourier phase
-                im01 = ax[1].imshow(aligned_phases_weighted, cmap='seismic', vmin=aligned_phases_norm.min(),
-                                    vmax=aligned_phases_norm.max())
+                im01 = ax[1].imshow(aligned_phases_weighted, cmap='seismic', vmin=aligned_phase.min(),
+                                    vmax=aligned_phase.max())
                 ax[1].set_title("aligned phase")
                 plt.colorbar(im01, ax=ax[1], fraction=0.046, pad=0.04)
                 ax[1].axis([theshape[0] // 2 - theshape[0] // 2 // zoom,
@@ -183,20 +137,49 @@ def phase_alignment_gerchberg_saxton(amplitude_filename = None,
                 plt.show()
             else:
                 print("Alignment of %d \r" % int((phase_idx + 1) / len(phase_filenames)),
-                     " % of images completed")
+                      " % of images completed")
         else:
             print("Alignment of %d \r" % int((phase_idx + 1) / len(phase_filenames)),
                   " % of images completed")
-        #
+    #
+    # compute the average of the aligned phases
+    print("averaging ")
+    aligned_phase_norm = aligned_phase / len_phase_filenames - int(symmetric_phase) * 0.5 * np.max(np.max(aligned_phase)) / len_phase_filenames
+    #
     #save aligned images
     filename_full = amplitude_filename[:-14] + '_phase.csv'
-    np.savetxt(filename_full, aligned_phases_norm, delimiter='\t')
+    np.savetxt(filename_full, aligned_phase_norm, delimiter='\t')
     print('Aligned phase distribution was saved as ', filename_full)
     #
+    return amplitude, phase, ref_coordinates, len_phase_filenames, aligned_phase, aligned_phase_norm
 
-
-
-def plot_reconstruction(amplitude_filename = None, phase_filename = None, delimiter = ',', zoom = 1, save_as_eps = False):
+def plot_reconstruction(amplitude_filename = None,
+                        phase_filename = None,
+                        delimiter = ',',
+                        zoom = 1,
+                        save_as_eps = False):
+    """
+    Plot aligned amplitude and phase images yielded by phase_alignment_gerchberg_saxton method
+    ---
+    Parameters
+    ---
+    amplitude_filename : list
+        Path to file containing amplitude distribution
+        Default is None
+    phase_filenames : list
+        Path to file with aligned phase distributions
+        Default is None
+    delimiter : str, optional
+        Delimiter in csv files
+        Default is '\t'
+    zoom: int, optional
+        Zoom factor to zoom into the 2D plot
+        Default is 1 (no zoom)
+    save_as_eps: bool, optional
+        If set to False, the images are only displayed
+        If set to True, the displayed image is saved as eps file
+        Default is False.
+    """
     #
     # read amplitude image
     if os.path.exists(amplitude_filename):
