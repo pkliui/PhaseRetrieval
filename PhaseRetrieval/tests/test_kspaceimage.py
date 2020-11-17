09078ebbb43f45794dc734359859a399bea07fff
pkliui <kliuiev@pm.me>
2020-11-17 14:13:40 +0200

    complemented kspaceimage tests

--- PhaseRetrieval/tests/test_kspaceimage.py
+++ PhaseRetrieval/tests/test_kspaceimage.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 import os
 import shutil, tempfile
+from skimage import io
 
 from ddt import ddt
 
@@ -30,51 +31,126 @@ class TestKSpaceImageClass(unittest.TestCase):
         shutil.rmtree(self.test_dir)
 
     def test_empty_arguments(self):
+        """
+        test the input arguments are existing and are all None
+        :return:
+        """
         for var in ['filename', 'delimiter', 'image']:
             self.assertIn(var, self.ks.__dict__)
             self.assertEqual(self.ks.__dict__[var], None)
 
     def test_read_from_csv(self):
+        """
+        test missing positional arguments
+        test to read some non-existing data
+        :return:
+        """
         with self.assertRaises(TypeError):
             self.ks.read_from_csv()
         with self.assertRaises(ValueError):
             self.ks.read_from_csv(filename='some_none_existing_data.csv',
                              delimiter = ',')
 
+    def test_read_from_csv_metadata(self):
+        """
+        test metadata
+        :return:
+        """
+        # save data in temp directory
+        files_in_dir = os.listdir(self.test_dir)
+        test_image = np.ones((3,3))
+        np.savetxt(os.path.join(self.test_dir, 'test_image.csv'), test_image, delimiter='\t')
+        #
+        # read saved data and check its metadata
+        self.ks.read_from_csv(filename=os.path.join(self.test_dir, 'test_image.csv'), delimiter = '\t')
+        #
+        self.assertEqual(self.ks.metadata['Image centred and padded?'], 'no')
+        self.assertEqual(self.ks.metadata['Background subtracted?'], 'no')
+
     def test_read_from_tif(self):
+        """
+        test to read some non-existing data
+        :return:
+        """
         with self.assertRaises(TypeError):
             self.ks.read_from_tif()
         with self.assertRaises(ValueError):
             self.ks.read_from_tif(filename='some_none_existing_data.tif')
 
+    def test_read_from_tif_metadata(self):
+        """
+        test metadata
+        :return:
+        """
+        # save data in temp directory
+        files_in_dir = os.listdir(self.test_dir)
+        test_image = np.ones((3,3))
+        io.imsave(os.path.join(self.test_dir, 'test_image.tif'), test_image.astype(np.float32))
+        #
+        # read saved data and check its metadata
+        self.ks.read_from_tif(filename=os.path.join(self.test_dir, 'test_image.tif'))
+        #
+        self.assertEqual(self.ks.metadata['Image centred and padded?'], 'no')
+        self.assertEqual(self.ks.metadata['Background subtracted?'], 'no')
+
     def test_plot_image(self):
+        """
+        test the case when an input image object is None
+        :return:
+        """
         self.ks.image = None
         with self.assertRaises(ValueError):
             self.ks.plot_image()
 
     def test_rotate_image(self):
+        """
+        test the case when an input image object is None
+        :return:
+        """
         self.ks.image = None
         with self.assertRaises(ValueError):
             self.ks.rotate_image()
 
     def test_flip_image(self):
+        """
+        test the case when an input image object is None
+        :return:
+        """
         self.ks.image = None
         with self.assertRaises(ValueError):
             self.ks.flip_image()
 
-    def test_centre_image_manually(self):
+    def test_centre_image_manually_empty(self):
+        """
+        test empty arguments
+        :return:
+        """
         self.ks.image = None
         with self.assertRaises(ValueError):
             self.ks.centre_image_manually()
-        #
-        self.ks.image = np.zeros((10,10))
+
+    def test_centre_image_manually_too_high_npixels_final(self):
+        """
+        test the final linear number of pixels npixels_final is exceeding image's linear dimensions
+        :return:
+        """
+        self.ks.image = np.ones((6,6))
         with self.assertRaises(ValueError):
-            self.ks.centre_image_manually(npixels_final = 5.0)
+            self.ks.centre_image_manually(npixels_final = 4.0)
         with self.assertRaises(ValueError):
-            self.ks.centre_image_manually(npixels_final = 5)
+            self.ks.centre_image_manually(npixels_final = 4)
         #
-        self.ks.centre_image_manually(npixels_final=20)
-        self.assertEqual(self.ks.image.shape[0],20)
+
+    def test_centre_image_manually(self):
+        """
+        test centering manually
+        :return:
+        """
+        self.ks.image = np.ones((6,6))
+        self.ks.image[0,0] = 0
+        self.ks.centre_image_manually(manual_centroid = (4, 4), npixels_final=8)
+        print(self.ks.image)
+        #self.assertEqual(self.ks.image.shape[0],20)
 
     def test_centre_image(self):
         self.ks.image = None
